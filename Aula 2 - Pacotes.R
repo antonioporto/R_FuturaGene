711d659a5ba7455afd00278bb461a6f3d9d2786e
Antonio Porto <42418473+antonioporto@users.noreply.github.com>
2024-09-10 21:34:05 -0300

    Add files via upload

---
+++ Aula 2 - Pacotes.R
@@ -0,0 +1,21 @@
+# Antonio Carlos Porto, 12.09.24
+# Aula 2
+
+
+#######################################################################################
+########################## Pacotes ####################################################
+#######################################################################################
+
+#### Instalando pacotes 
+
+install.packages("fBasics")
+install.packages("ggplot2")
+install.packages("car")
+install.packages("ExpDes")
+install.packages("agricolae")
+install.packages("asbio")
+install.packages("latticeExtra")
+
+
+
+
